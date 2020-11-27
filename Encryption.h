45c3be07eaf08aa8e4e4925d1a22f8605b4db958
Alexander1248 <75137404+Alexander1248@users.noreply.github.com>
2020-11-27 21:23:46 +0300

    Add files via upload

--- Encryption.h
+++ Encryption.h
@@ -1,26 +1,26 @@
-#include <math.h>
+п»ї#include <math.h>
 #include <ctime>
 #include <string>
 
 void SHP_1(char*& hashed, char* data, int dataLen, int hashLen, int cost) {
 	hashed = new char[hashLen];
 	int k = 0;
-	//Создание структуры данных хэша
+	//Creating a hash data structure
 	for (int i = 0; i < hashLen; i++) {
 		hashed[i] = data[k];
 		k++;
 		if (k >= dataLen) k = 0;
 	}
-	//Обмен значений с образованием первичной структуры
+	// Exchange of values to form a primary structure
 	for (int i = 0; i < hashLen; i++) {
 		char b = hashed[hashLen - 1];
 		hashed[hashLen - 1] = hashed[hashed[i] % hashLen];
 		hashed[hashed[i] % hashLen] = b;
 	}
 	long sum = 0;
-	//Волновое редактирование хэша (тут и появляется эффект перестройки хэша из-за одного знака)
+	//Wave editing of the hash (this is where the effect of rebuilding the hash due to a single character appears)
 	if (hashLen >= dataLen) {
-		//Волновое редактирование при меньшей длине входящих данных по сравнению с длиной самого хэша
+		// Wave editing with a shorter length of incoming data compared to the length of the hash itself
 		for (int epoch = 0; epoch < cost; epoch++) {
 			for (int j = 0; j < hashLen; j++) {
 				for (int i = 0; i < j; i++) {
@@ -34,12 +34,12 @@ void SHP_1(char*& hashed, char* data, int dataLen, int hashLen, int cost) {
 		}
 	}
 	else {
-		//Волновое редактирование при большей длине входящих данных по сравнению с длиной самого хэша
+		// Wave editing when the input data is longer than the hash itself
 		for (int epoch = 0; epoch < cost; epoch++) {
 			for (int j = 0; j < hashLen; j++) {
 				for (int i = 0; i < j; i++) {
 					sum += hashed[i];
-					//Компенсация переизбытка данных
+					//Payment of excess data
 					for (int d = i; d < j; d++) {
 						sum += data[d];
 					}
@@ -49,7 +49,7 @@ void SHP_1(char*& hashed, char* data, int dataLen, int hashLen, int cost) {
 				}
 				for (int i = j + 1; i < hashLen; i++) {
 					sum -= hashed[i];
-					//Компенсация переизбытка данных
+					//Payment of excess data
 					for (int d = i; d < j + 1; d++) {
 						sum += data[d];
 					}
@@ -79,7 +79,7 @@ void RSAKeyGen(unsigned int openKey[2], unsigned int secretKey[2]) {
 	const int PNum[54] = { 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251 };
 	bool f = false;
 	unsigned int n1, n2;
-	//Нахождение псевдопростого n1
+	//Finding the pseudo-simple n1
 	while (f != true) {
 		n1 = 1000 + rand() % 64535;
 		for (int i = 0; i < 54; i++) if (n1 % PNum[i] != 0) f = true;
@@ -93,7 +93,7 @@ void RSAKeyGen(unsigned int openKey[2], unsigned int secretKey[2]) {
 		}
 	}
 	f = false;
-	//Нахождение псевдопростого n2
+	//Finding the pseudo-simple n2
 	while (f != true) {
 		n2 = 1000 + rand() % 64535;
 		for (int i = 0; i < 54; i++) if (n2 % PNum[i] != 0) f = true;
@@ -106,7 +106,7 @@ void RSAKeyGen(unsigned int openKey[2], unsigned int secretKey[2]) {
 			if (k >= trunc(log2(n1))) f = true;
 		}
 	}
-	//Вычисление ключа RSA алгоритма шифрования
+	//Calculating the RSA key of the encryption algorithm
 	unsigned int n = n1 * n2;
 	unsigned int nEyler = (n1 - 1) * (n2 - 1);
 	unsigned int e = 1000 + rand() % (nEyler - 1000);
@@ -135,7 +135,7 @@ void RSADecryption(unsigned int decrypted, unsigned int secretKey[2], int messag
 }
 //...
 
-//Трансформатор значений первого поколения
+//Transformer values of the first generation
 void HPMv1(char* data, int dataLen) {
 	for (int i = 0; i < dataLen; i++) {
 		int j = data[i] % dataLen;
@@ -145,7 +145,7 @@ void HPMv1(char* data, int dataLen) {
 	}
 }
 
-//Трансформатор значений второго поколения
+//Transformer values of the second generation
 void HPMv2(char* data, int dataLen) {
 	for (int j = 1; j <= dataLen; j++) {
 		for (int i = j; i < dataLen; i += j) {
@@ -156,7 +156,7 @@ void HPMv2(char* data, int dataLen) {
 	}
 }
 
-//Трансформатор значений третьего поколения
+//Transformer values of the third generation
 void HPMv3(char* data, int dataLen) {
 	char* hash;
 	for (int i = 0; i < dataLen; i++) {
@@ -168,23 +168,23 @@ void HPMv3(char* data, int dataLen) {
 	}
 }
 
-//Простой шифрантор на основе шифра Виженера
+////A simple encryptor based on the Vigener cipher
 void SEPEncryption(char data[],char key[],int len,bool keyGen) {
-	//Генерация ключа(если keyGen == true)
+	//Р“РµРЅРµСЂР°С†РёСЏ РєР»СЋС‡Р°(РµСЃР»Рё keyGen == true)
 	if (keyGen == true) {
 		srand(int(data) + len);
 		for (int i = 0; i < len; i++) key[i] = 33 + rand() % 223;
 	}
-	//Шифратор сдвигом
+	// Shift encoder
 	for (int i = 0; i < len; i++) {
 		data[i] += key[i];
 		if (data[i] > 255) data[i] -= 223;
 	}
 }
 
-//Простой дефрантор с шифра Виженера
+////A simple decoder with the VigenГЁre cipher
 void SEPDecryption(char data[], char key[], int len) {
-	//Дефратор обратным сдвигом
+	//Reverse shift decoder
 	for (int i = 0; i < len; i++) {
 		data[i] -= key[i];
 		if (data[i] < 33) data[i] += 233;
@@ -193,44 +193,44 @@ void SEPDecryption(char data[], char key[], int len) {
 
 
 void SHP_2(char*& hashed, char* data,int dataLen, int hashLen) {
-	// Первичная обработка 
+	//Primary processing
 	HPMv2(data, dataLen);
 
-	//Первичное хэширование с последующей обработкой
+	//Primary hashing with subsequent processing
 	char* hash = new char[64];
 	SHP_1(hash, data, dataLen, 64, 10);
 	HPMv1(hash, 64);
 
-	//Повторное хэширование с двухступенчатой оброботкой
+	//Repeated hashing with two-step processing
 	char* hash1 = new char[256];
 	SHP_1(hash1, hash, 64, 256, 10);
 	HPMv1(hash1, 256);
 	HPMv2(hash1, 256);
 
-	//Финальное хеширование с обработкой
+	//Final hashing with processing
 	SHP_1(hashed, hash1, 256, hashLen, 10);
 	HPMv2(hashed, hashLen);
 }
 
 void SHP_3(char*& hashed, char* data, int dataLen, int hashLen) {
-	// Первичная обработка
+	//Primary processing
 	HPMv3(data, dataLen);
 
-	//Слабое хэширование с последующей двухэтапной обработкой
-	//(не улучшенный метод хеширования из-за низкой скорости улучшенного метода по сравнению с классическим)
+	////Weak hashing followed by two-step processing
+	//(not an improved hashing method due to the low speed of the improved method compared to the classic one)
 	char* hash = new char[64];
 	SHP_1(hash, data, dataLen, 64, 10);
 	HPMv1(hash, 64);
 	HPMv3(hash, 256);
 
-	//Повторное хеширование улучшенным алгоритмом с трехэтапной обработкой
+	//Re-hashing with an improved algorithm with three-step processing
 	char* hash1 = new char[256];
 	SHP_2(hash1, hash, 64, 256);
 	HPMv1(hash1, 256);
 	HPMv2(hash1, 256);
 	HPMv3(hash1, 256);
 
-	//Финальное хэширование улучшенным алгоритмом с двухэтапной обработкой
+	////Final hashing with an improved algorithm with two-step processing
 	SHP_2(hashed, hash1, 256, hashLen);
 	HPMv3(hashed, hashLen);
 	HPMv2(hash1, 256);
